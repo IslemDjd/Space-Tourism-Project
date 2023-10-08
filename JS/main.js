d70a4ee13da2d2569930532bc8cbe32f3bc64c1c
Islem Dj <islemas23@gmail.com>
2023-10-08 17:05:06 +0100

    Separate Js file

--- JS/main.js
+++ JS/main.js
@@ -12,27 +12,7 @@ if(pageName !== "index.html"){
 
 console.log(pageName);
 
-const toggleButton = document.getElementsByClassName('toggle')[0];
-const nav_btn = document.getElementsByClassName('navElements')[0];
-var closePNG = document.getElementById("close");
 
-toggleButton.addEventListener('click',() =>{
-    nav_btn.classList.toggle('active');
-    // const activeNavElements = document.getElementsByClassName('active')[0];
-    // activeNavElements.style.display = "block";
-    closePNG.style.display = "block";
-    toggleButton.style.display = "none";
-
-});
-
-closePNG.addEventListener('click',() =>{
-    // const activeNavElements = document.getElementsByClassName('active')[0];
-    // activeNavElements.style.display = "none";
-    nav_btn.classList.toggle('active');
-    closePNG.style.display = "none";
-    toggleButton.style.display = "block";
-
-});
 
 function pageReload(){
     window.location.reload();  
